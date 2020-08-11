6432e69c5f06753fbd5e3e39ec3251c2aa9e8ade
Brad <bcoffield@gmail.com>
2020-08-11 15:29:52 -0600

    added eleventy.js

---
+++ .eleventy.js
@@ -0,0 +1,26 @@
+module.exports = function (eleventyConfig) {
+    eleventyConfig.addPassthroughCopy("assets");
+    eleventyConfig.addPassthroughCopy("images");
+    // eleventyConfig.setUseGitIgnore(false);
+    // eleventyConfig.addPassthroughCopy("css");
+    eleventyConfig.setTemplateFormats([
+      "md",
+      "css",
+      "html",
+      "jpg",
+      "doc",
+      "png",
+      "gif",
+      "ico",
+      "js"
+    ]);
+  
+    return {
+      dir: {
+        input: "./src", 
+        output: "_site",
+      },
+       
+    };
+  };
+  
